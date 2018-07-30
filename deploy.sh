2f28717ccbc3f7098f3a2a7ccb5e9f48340bd539
whatwewant <tobewhatwewant@gmail.com>
2018-07-30 18:28:59 +0800

    :green_heart:

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "deploy to gh-pages."
+
+cp -rf dist/* gh-pages
+cp CNAME gh-pages
+cd gh-pages
+git add --all .
+git commit -a -m ':sparkles:(TravisCI) automatically update from travis_ci ${TRAVIS_TAG}'
+git push --quiet "https://${GITHUB_TOKEN}@github.com/${TRAVIS_REPO_SLUG}" gh-pages:gh-pages
